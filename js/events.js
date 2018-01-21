856aaf221c0c8474a1fc5706466deb5fd1667133
River Magnetic <magneticcontinuum@gmail.com>
2018-01-21 18:13:55 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,15 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt(){
+  $('p').on("click", function(){
+   alert("Hey!");
+});
+}
+
+function frameIt(){
+  $('img').on("load", function(){
+   'img'.addClass("tasty")
+});
+}
